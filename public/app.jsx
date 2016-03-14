743740e202a06315e531354db216b3595d842a02
David Luecke <daff@neyeon.com>
2016-03-13 21:02:09 -0700

    Adding React chat client

---
+++ public/app.jsx
@@ -0,0 +1,160 @@
+// A placeholder image if the user does not have one
+const PLACEHOLDER = 'http://placehold.it/60x60';
+// An anonymous user if the message does not have that information
+const dummyUser = {
+  image: PLACEHOLDER,
+  email: 'Anonymous'
+};
+
+// Establish a Socket.io connection
+const socket = io();
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+const app = feathers()
+  .configure(feathers.socketio(socket))
+  .configure(feathers.hooks())
+  // Use localStorage to store our login token
+  .configure(feathers.authentication({
+    storage: window.localStorage
+  }));
+
+const UserList = React.createClass({
+  logout() {
+    app.logout().then(() => window.location.href = '/login.html');
+  },
+
+  render() {
+    const users = this.props.users;
+
+    return <aside className="sidebar col col-3 flex flex-column flex-space-between">
+      <header className="flex flex-row flex-center">
+        <h4 className="font-300 text-center">
+          <span className="font-600 online-count">{users.length}</span> users
+        </h4>
+      </header>
+
+      <ul className="flex flex-column flex-1 list-unstyled user-list">
+        {users.map(user =>
+          <li>
+            <a className="block relative" href="#">
+              <img src={user.image || PLACEHOLDER} className="avatar" />
+              <span className="absolute username">{user.email}</span>
+            </a>
+          </li>
+        )}
+      </ul>
+      <footer className="flex flex-row flex-center">
+        <a href="#" className="logout button button-primary" onClick={this.logout}>Sign Out</a>
+      </footer>
+    </aside>;
+  }
+});
+
+const ComposeMessage = React.createClass({
+  getInitialState() {
+    return { text: '' };
+  },
+
+  updateText(ev) {
+    this.setState({ text: ev.target.value });
+  },
+
+  sendMessage(ev) {
+    app.service('messages').create(this.state)
+      .then(() => this.setState({ text: '' }))
+    ev.preventDefault();
+  },
+
+  render() {
+    return <form className="flex flex-row flex-space-between" onSubmit={this.sendMessage}>
+      <input type="text" name="text" className="flex"
+        value={this.state.text} onChange={this.updateText} />
+      <button className="button-primary" type="submit">Send</button>
+    </form>;
+  }
+});
+
+const MessageList = React.createClass({
+  render() {
+    const users = this.props.users;
+    const messages = this.props.messages;
+    const renderMessage = message => {
+      const user = users.find(current => current._id === message.userId) || dummyUser;
+
+      return <div className="message flex flex-row">
+        <img src={user.image || PLACEHOLDER} alt={user.email} className="avatar" />
+        <div className="message-wrapper">
+          <p className="message-header">
+            <span className="username font-600">{user.email}</span>
+            <span className="sent-date font-300">
+              {moment(message.createdAt).format('MMM Do, hh:mm:ss')}
+            </span>
+          </p>
+          <p className="message-content font-300">
+            {message.text}
+          </p>
+        </div>
+      </div>;
+    }
+
+    return <main className="chat flex flex-column flex-1 clear">
+      {messages.map(renderMessage)}
+    </main>;
+  }
+});
+
+const ChatApp = React.createClass({
+  getInitialState() {
+    return {
+      users: [],
+      messages: []
+    };
+  },
+
+  componentDidMount() {
+    const userService = app.service('users');
+    const messageService = app.service('messages');
+
+    // Find all users initially
+    userService.find().then(page => this.setState({ users: page.data }));
+    // Listen to new users so we can show them in real-time
+    userService.on('created', user => this.setState({
+      users: this.state.users.concat(user)
+    }));
+
+    // Find the last 10 messages
+    messageService.find({
+      query: {
+        $sort: { createdAt: -1 },
+        $limit: this.props.limit || 10
+      }
+    }).then(page => this.setState({ messages: page.data.reverse() }));
+    // Listen to newly created messages
+    messageService.on('created', message => this.setState({
+      messages: this.state.messages.concat(message)
+    }));
+  },
+
+  render() {
+    return <div className="flex flex-row flex-1 clear">
+      <UserList users={this.state.users} />
+      <div className="flex flex-column col col-9">
+        <MessageList users={this.state.users} messages={this.state.messages} />
+        <ComposeMessage />
+      </div>
+    </div>
+  }
+});
+
+app.authenticate().then(() => {
+  ReactDOM.render(<div id="app" className="flex flex-column">
+    <header className="title-bar flex flex-row flex-center">
+      <div className="title-wrapper block center-element">
+        <img className="logo" src="http://feathersjs.com/img/feathers-logo-wide.png" alt="Feathers Logo" />
+        <span className="title">Chat</span>
+      </div>
+    </header>
+
+    <ChatApp />
+  </div>, document.body);
+}).catch(() => window.location.href = '/login.html');
---
+++ public/jquery/app.js
@@ -0,0 +1,100 @@
+$(function() {
+  // A placeholder image if the user does not have one
+  const PLACEHOLDER = 'http://placehold.it/60x60';
+  // An anonymous user if the message does not have that information
+  const dummyUser = {
+    image: PLACEHOLDER,
+    email: 'Anonymous'
+  };
+  // The total number of users
+  let userCount = 0;
+
+  function addUser(user) {
+    // Update the number of users
+    $('.online-count').html(++userCount);
+    // Add the user to the list
+    $('.user-list').append(`<li>
+      <a class="block relative" href="#">
+        <img src="${user.image || PLACEHOLDER}" alt="" class="avatar">
+        <span class="absolute username">${user.email}</span>
+      </a>
+    </li>`);
+  }
+
+  // Renders a new message and finds the user that belongs to the message
+  function addMessage(message, users) {
+    // Find the user belonging to this message or use the anonymous user if not found
+    const user = users.find(current => current._id === message.userId) || dummyUser;
+
+    $('.chat').append(`<div class="message flex flex-row">
+      <img src="${user.image || PLACEHOLDER}" alt="${user.email}" class="avatar">
+      <div class="message-wrapper">
+        <p class="message-header">
+          <span class="username font-600">${user.email}</span>
+          <span class="sent-date font-300">${moment(message.createdAt).format('MMM Do, hh:mm:ss')}</span>
+        </p>
+        <p class="message-content font-300">${message.text}</p>
+      </div>
+    </div>`);
+  }
+
+  // Establish a Socket.io connection
+  const socket = io();
+  // Initialize our Feathers client application through Socket.io
+  // with hooks and authentication.
+  const app = feathers()
+    .configure(feathers.socketio(socket))
+    .configure(feathers.hooks())
+    // Use localStorage to store our login token
+    .configure(feathers.authentication({
+      storage: window.localStorage
+    }));
+
+  app.authenticate().then(() => {
+    // Get the Feathers services we want to use
+    const userService = app.service('users');
+    const messageService = app.service('messages');
+
+    $('#send-message').on('submit', function(ev) {
+      // This is the message text input field
+      const input = $(this).find('[name="text"]');
+
+      // Create a new message and then clear the input field
+      messageService.create({
+        text: input.val()
+      }).then(message => input.val(''));
+
+      ev.preventDefault();
+    });
+
+    $('.logout').on('click', function() {
+      app.logout().then(() => window.location.href = '/login.html');
+    });
+
+    // Find all users
+    userService.find().then(page => {
+      const users = page.data;
+      // An addMessage wrapper what also passes the list of users
+      const createMessage = message => addMessage(message, users);
+
+      // Add every user to the list
+      users.forEach(addUser);
+
+      // Find the latest 10 messages. They will come with the newest first
+      // which is why we have to reverse before adding them
+      messageService.find({
+        query: {
+          $sort: { createdAt: -1 },
+          $limit: 10
+        }
+      }).then(page => page.data.reverse().forEach(createMessage));
+
+      // Listen to created events and add the new message in real-time
+      messageService.on('created', createMessage);
+    });
+    // We will also see when new users get created in real-time
+    userService.on('created', addUser);
+  })
+  // On errors we just redirect back to the login page
+  .catch(error => window.location.href = '/login.html');
+});
