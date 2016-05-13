e8e1ff185fcbc53d29f8b1ed274c1ea3f8828fbc
Diego Vieira <diego@protos.inf.br>
2016-05-12 22:02:45 -0300

    feat(build): make it compatible with heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || app.get('port');
 const server = app.listen(port);
 
 server.on('listening', () =>
